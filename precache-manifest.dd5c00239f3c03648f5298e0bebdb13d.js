eeb7cfa5a004ccc28361c60f55694e59b30951c0
swap-karan <swap.guleria@gmail.com>
2020-03-05 17:11:46 +0530

    Updates

---
+++ precache-manifest.dd5c00239f3c03648f5298e0bebdb13d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176dbbde4f1f42330ca56c532cf9bb10",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "abc6abb95175da07b3f7",
+    "url": "/React/static/css/main.6a3169fb.chunk.css"
+  },
+  {
+    "revision": "e6e4685ebaacaada88a1",
+    "url": "/React/static/js/2.d142bc84.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React/static/js/2.d142bc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc6abb95175da07b3f7",
+    "url": "/React/static/js/main.8858c908.chunk.js"
+  },
+  {
+    "revision": "3b70f4daf685974de05b",
+    "url": "/React/static/js/runtime-main.5e866e1b.js"
+  },
+  {
+    "revision": "ca3b51ed23dd34de1a8b8ff7a60cb1a8",
+    "url": "/React/static/media/1.ca3b51ed.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
